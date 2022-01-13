c3e15f0d609473c29a061867fd48ce8f386d8115
April Skrine <april.skrine@gmail.com>
2022-01-13 15:53:42 -0600

    commit tree 09b2337e5cb6b4b690f5f9952ef94df0c5af52d1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(someArray, someString) {
+    const newDrivers =  someArray.filter( (thisThing) => thisThing.toUpperCase() === someString.toUpperCase() )
+    return newDrivers;
+}
+
+function fuzzyMatch(someArray, someString) {
+    const namesWithLetter = someArray.filter( (thisThing) => thisThing.substring(0,1) === someString.substring(0,1) )
+    return namesWithLetter;
+}
+
+function matchName(someArray, someString) {
+    const peopleWithHometown = someArray.filter ( (thisThing) => thisThing.name === someString )
+    return peopleWithHometown;
+}
